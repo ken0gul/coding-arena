46ddcec38437b7fde19b2d91bd584cbe092cd891
Ogulcan Kendirci <93669966+ken0gul@users.noreply.github.com>
2023-06-18 12:04:09 -0400

    Update Editor.jsx

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -116,7 +116,7 @@ const Editor = ({ setOutput }) => {
     const code = editor.getValue();
 
     try {
-      const response = await fetch("https://localhost:3001/execute", {
+      const response = await fetch("https://coding-arena-api-production.up.railway.app/execute", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
