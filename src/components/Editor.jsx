46a2a852ba177c73f5e230e18a662facedb27fe2
Ogulcan Kendirci <93669966+ken0gul@users.noreply.github.com>
2023-06-18 17:01:59 -0400

    Update Editor.jsx

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -118,7 +118,6 @@ const Editor = ({ setOutput }) => {
     try {
       const response = await fetch("https://coding-arena-api-production.up.railway.app/execute", {
         method: "POST",
-        mode:"cors",
         headers: {
           "Content-Type": "application/json",
         },
