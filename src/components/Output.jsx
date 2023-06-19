6bf9b0e6e8b6f07bcb96e6cab78a2d009d5a7064
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-06-18 20:02:34 -0400

    updated expected output

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -8,7 +8,11 @@ export default function Output({ output }) {
         <li>Create a method to reverse a given string("CodersCampus")</li>
         <li>Call the method in the main method of Code class</li>
         <li>Print out the result</li>
-        <li>Expected Output: "supmaCsredoC"</li>
+        <li>
+          <h2>
+            Expected Output: <strong>"supmaCsredoC"</strong>
+          </h2>
+        </li>
       </ul>
       <h2>
         Output: <br />
